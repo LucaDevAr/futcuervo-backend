2590e7178e06cf236c1887f7a082bfe1581163f3
Luca Almiron <lucadev.arg@gmail.com>
2025-11-06 17:08:33 -0300

    feat(deploy): prepare backend for deployment on Railway

--- src/app.js
+++ src/app.js
@@ -30,7 +30,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(passport.initialize());
